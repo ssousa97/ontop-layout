8d4aedce6625c3e09da99a3f3503789e086f79b5
Samuel Sousa <samuelsousa064@gmail.com>
2022-04-28 10:29:24 -0300

    adding fonts, topbar and contracts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContractsComponent } from './contracts/contracts.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractsComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
