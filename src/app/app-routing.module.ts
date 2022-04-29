77d79d9975d2fdad18a5a3a55d1597a426e1be86
Samuel Sousa <samuelsousa064@gmail.com>
2022-04-28 23:57:36 -0300

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContractsComponent } from './contracts/contracts.component';
-import { DocumentsComponent } from './documents/documents.component';
 
 const routes: Routes = [
 
@@ -9,10 +8,6 @@ const routes: Routes = [
     path: '',
     component: ContractsComponent
   },
-  {
-    path: 'documents',
-    component: DocumentsComponent
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { ContractsComponent } from './contracts/contracts.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContractsComponent,
     TopbarComponent,
-    DocumentsComponent
   ],
   imports: [
     BrowserModule,
